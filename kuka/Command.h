387280da3924c1f24835cf71aedc23a7b0b807a1
Nathan Goller-Deitsch <nathan@quartzic.co>
2021-09-30 07:58:41 -0400

    Add SPL, SPLINE, ENDSPLINE

--- kuka/Command.h
+++ kuka/Command.h
@@ -18,7 +18,7 @@ class Command {
   virtual const std::string compileKRL() = 0;
 };
 
-class Comment : public Command{
+class Comment : public Command {
  public:
   const std::string compileKRL() override;
   Comment(std::string comment);
@@ -45,51 +45,19 @@ class PTP : public Command {
   Frame frame;
   PTP(Frame);
 };
-class Draw2DPoint {
- public:
-  float x;
-  float y;
-  Draw2DPoint(float x, float y);
-};
-class Draw2DPenUp : public Command {
+class SPL : public Command {
  public:
   const std::string compileKRL() override;
-  Draw2DPenUp();
+  Frame frame;
+  SPL(Frame);
 };
-class Draw2DPenDown : public Command {
+class SPLINE : public Command {
  public:
   const std::string compileKRL() override;
-  Draw2DPenDown();
 };
-class Draw2DLine : public Command {
+class ENDSPLINE : public Command {
  public:
-  float x1;
-  float y1;
-  float x2;
-  float y2;
-  Draw2DLine(float x1, float y1, float x2, float y2);
   const std::string compileKRL() override;
 };
-class Draw2DSquare : public Command {
- public:
-  float x;
-  float y;
-  float length;
-  float width;
-  Draw2DSquare(float x, float y, float length, float width);
-  const std::string compileKRL() override;
-};
-class Draw2DPath : public Command {
- public:
-  std::vector<Draw2DPoint> points;
-  Draw2DPath(const std::vector<Draw2DPoint>& points);
-  const std::string compileKRL() override;
-};
-class Draw2DSpline : public Command {
- public:
-  std::vector<Draw2DPoint> points;
-  Draw2DSpline(const std::vector<Draw2DPoint>& points);
-  const std::string compileKRL() override;
-};
-}
+}  // namespace Kuka
 #endif  // SYNTH_COMMAND_H
